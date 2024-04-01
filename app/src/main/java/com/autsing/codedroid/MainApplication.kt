14332e7b32cf932aba86b8fb998e49f34f37ddf3
AuTsing <763025696@qq.com>
2024-04-02 01:42:50 +0800

    rm legacy project

--- app/src/main/java/com/autsing/codedroid/MainApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.autsing.codedroid
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-const val TAG = "codedroid"
-
-@HiltAndroidApp
-class MainApplication : Application()
